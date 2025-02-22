b41a4effb8cb28fece5abce8636a26632fbd8d87
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-02-22 17:45:46 +0500

    update database.js

--- config/database.js
+++ config/database.js
@@ -1,43 +1,37 @@
 const path = require('path');
 
-// const parse = require("pg-connection-string").parse;
-// const config = parse(process.env.DATABASE_URL);
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host: config.host,
-//       port: config.port,
-//       database: config.database,
-//       user: config.user,
-//       password: config.password,
-//       ssl: {
-//         rejectUnauthorized: false, 
-//       },
-//     },
-//     pool: {
-//       min: 0,
-//       max: 10,
-//     },
-//     acquireConnectionTimeout: 60000,
-//   },
-// });
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: {
-        rejectUnauthorized: false, // For self-signed certificates
-      },
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: env.bool('DATABASE_SSL', true) ? { rejectUnauthorized: false } : false,
     },
     debug: false,
   },
 });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'strapi'),
+//       user: env('DATABASE_USERNAME', 'strapi'),
+//       password: env('DATABASE_PASSWORD', 'strapi'),
+//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//       ssl: {
+//         rejectUnauthorized: false, // For self-signed certificates
+//       },
+//     },
+//     debug: false,
+//   },
+// });
